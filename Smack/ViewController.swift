38b1552673954e74287b882081da424ad5c51d62
Bilgutei Erdenebayar <beke0130@gmail.com>
2020-02-27 16:56:22 +0800

    Initial Commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Bilgutei on 2/27/20.
+//  Copyright © 2020 Bilgutei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
