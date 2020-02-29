2bc440444ae9459f6a1f68b65cd28047403bad5d
Bilgutei Erdenebayar <beke0130@gmail.com>
2020-02-29 23:07:47 +0800

    End of lesson 04

---
+++ LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Bilgutei on 2/29/20.
+//  Copyright © 2020 Bilgutei. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Bilgutei on 2/29/20.
+//  Copyright © 2020 Bilgutei. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
